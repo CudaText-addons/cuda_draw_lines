aa9262870dd75a65e76f308abf2ed9b7dde27b5f
Alexey-t <support@uvviewsoft.com>
2018-11-04 20:30:56 +0300

    work

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ MSG_OFF = 'Turned off'
 MSG_ONE = 'Shift+arrows: ├───┤' 
 MSG_TWO = 'Shift+arrows: ╠═══╣'
 
-CHAR_PROP = {
+PROPS = {
   '─': (1, 0, 1, 0),
   '│': (0, 1, 0, 1),
   '┌': (0, 0, 1, 1),
@@ -77,6 +77,49 @@ class Command:
         else:
             ed.set_text_line(y, s+' '*(x-len(s))+ch)
     
+    
+    def near_props(self, x, y):
+        """
+        gets (px1, py1, px2, py2): where each int is
+        0: no line, 1: single line, 2: double line
+        """
+    
+        s = ed.get_text_line(y)
+    
+        if 0<=x-1<len(s):
+            p = PROPS.get(s[x-1])
+            px1 = p[2] if p else 0
+        else:
+            px1 = 0
+            
+        if 0<=x+1<len(s):
+            p = PROPS.get(s[x+1])
+            px2 = p[0] if p else 0
+        else:
+            px2 = 0
+            
+        if y==0:
+            py1 = 0
+        else:
+            s = ed.get_text_line(y-1)
+            if 0<=x<len(s):
+                p = PROPS.get(s[x])
+                py1 = p[3] if p else 0
+            else:
+                py1 = 0
+                
+        if y+1>=ed.get_line_count():
+            py2 = 0
+        else:
+            s = ed.get_text_line(y+1)
+            if 0<=x<len(s):
+                p = PROPS.get(s[x])
+                py2 = p[1] if p else 0
+            else:
+                py2 = 0
+                
+        return (px1, py1, px2, py2)
+        
 
     def on_key(self, ed_self, key, state):
 
@@ -95,23 +138,27 @@ class Command:
         if key==kk.VK_LEFT:
             if x==0: return
             ch = '─'
+            print(self.near_props(x, y))
             self.repl(x, y, ch)
             ed.cmd(cmds.cCommand_KeyLeft)
 
         elif key==kk.VK_UP:
             if y==0: return
             ch = '│'
+            print(self.near_props(x, y))
             self.repl(x, y, ch)
             ed.cmd(cmds.cCommand_KeyUp)
 
         elif key==kk.VK_RIGHT:
             ch = '─'
+            print(self.near_props(x, y))
             self.repl(x, y, ch)
             ed.cmd(cmds.cCommand_KeyRight)
 
         elif key==kk.VK_DOWN:
             if y==ed.get_line_count()-1: return
             ch = '│'
+            print(self.near_props(x, y))
             self.repl(x, y, ch)
             ed.cmd(cmds.cCommand_KeyDown)
             
