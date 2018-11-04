986d60a78e9028844b3cb1c2f2171275a30fef37
Alexey-t <support@uvviewsoft.com>
2018-11-04 19:38:38 +0300

    f9 key

--- __init__.py
+++ __init__.py
@@ -3,11 +3,9 @@ from cudatext import *
 import cudatext_keys as kk
 import cudatext_cmd as cmds
 
-MODE_MSG = [
-    'Turned off', 
-    'Shift+arrows: ├───┤', 
-    'Shift+arrows: ╠═══╣'
-    ]
+MSG_OFF = 'Turned off' 
+MSG_ONE = 'Shift+arrows: ├───┤' 
+MSG_TWO = 'Shift+arrows: ╠═══╣'
 
 CHAR_PROP = {
   '─': (1, 0, 1, 0),
@@ -53,16 +51,22 @@ CHAR_PROP = {
 }
 
 class Command:
-    mode = 0
+    act = False
+    mode = False
     
     def toggle(self):
     
-        self.mode = (self.mode+1)%3
-        ev = 'on_key' if self.mode>0 else ''
+        self.act = not self.act
+        ev = 'on_key' if self.act else ''
 
-        # react only to 4 arrow key codes: 37..40
-        app_proc(PROC_SET_EVENTS, 'cuda_draw_lines;'+ev+';;37,38,39,40')
-        msg_status('[Draw Lines] '+MODE_MSG[self.mode])
+        # react only to 4 arrow key codes: 37..40, 120=VK_F9
+        app_proc(PROC_SET_EVENTS, 'cuda_draw_lines;'+ev+';;37,38,39,40,120')
+        self.status()
+
+
+    def status(self):
+    
+        msg_status('[Draw Lines] '+(MSG_OFF if not self.act else MSG_TWO if self.mode else MSG_ONE))
 
 
     def repl(self, x, y, ch):
@@ -75,6 +79,11 @@ class Command:
     
 
     def on_key(self, ed_self, key, state):
+
+        if state=='' and key==kk.VK_F9:
+            self.mode = not self.mode
+            self.status()
+            return False 
 
         # require Shift key
         if 's' not in state: return
